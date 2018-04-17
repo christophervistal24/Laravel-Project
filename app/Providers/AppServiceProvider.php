3b651911d26f2000ac6645bcac1b45283b123bd0
Christopher <christophervistal24@gmail.com>
2018-04-17 23:43:39 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,23 +7,49 @@ use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+    // /**
+    //  * Bootstrap any application services.
+    //  *
+    //  * @return void
+    //  */
+    // public function boot()
+    // {
+    //     Schema::defaultStringLength(191);
+    // }
+
+    // /**
+    //  * Register any application services.
+    //  *
+    //  * @return void
+    //  */
+    // public function register()
+    // {
+    //     //
+    // }
+
+
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    * Bootstrap any application services.
+    *
+    * @return void
+    */
+    public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191);
+        if(env('REDIRECT_HTTPS')){
+            $url->formatScheme('https');
+        }
     }
 
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
+    * Register any application services.
+    *
+    * @return void
+    */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')){
+            $this->app['request']->server->set('HTTPS',true);
+        }
     }
+
 }
